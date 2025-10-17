1166647ffa1048124ecb93a4e24da292a59734ed
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-17 16:26:57 +0000

    Add: Task 4 - Line Up - Solution

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" A script that add two arrays element-wise"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    A function that adds 2 arrays element-wise
+    """
+    if len(arr1) != len(arr2):
+        return None
+
+    result = []
+
+    for i in range(len(arr1)):
+        result.append(arr1[i] + arr2[i])
+
+    return result
