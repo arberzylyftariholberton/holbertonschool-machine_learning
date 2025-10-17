d9c1d00838e95c07067e78a2dcca0d6b41d56e16
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-17 17:15:22 +0000

    Add: Task 6 Concatenation --- solution

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A script that does the concatenation of 2 arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    A function that returns the concatenation of 2 arrays
+    """
+    result = []
+
+    result = arr1 + arr2
+
+    return result
