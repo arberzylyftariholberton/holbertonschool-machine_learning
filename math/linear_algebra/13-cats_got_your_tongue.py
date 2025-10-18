0de3197e110425527d874fd7c01e574eedcf580e
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-18 13:59:36 +0000

    Concatenation of 2 matrices along a specific axis --- Task Solution

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" A script that concatenates 2 matrices along a specific axis """
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    A function that returns concatenation of two matrices along a specific axis
+    """
+
+    return np.concatenate((mat1, mat2), axis=axis)
