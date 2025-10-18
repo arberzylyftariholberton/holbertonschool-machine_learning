19dd22dcd1417a1e62f1ba0b7eb401c3f8c6ef5b
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-18 14:04:57 +0000

    Multiplication of 2 matrices with Numpy-- Last Mandatory Task -- Solution

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""A script that performs a matrix multiplication """
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    A function that returns the multiplying of two matrices
+    """
+
+    return np.matmul(mat1, mat2)
