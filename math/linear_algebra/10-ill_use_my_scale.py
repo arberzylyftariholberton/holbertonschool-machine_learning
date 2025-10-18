e6c32bbc19e716d99be2fad96e057c8b0ec57a8f
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-18 13:07:32 +0000

    Add: Matrix shape - task solution

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""A script that calculates the shape of an array """
+
+import numpy as np
+
+
+def np_shape(matrix):
+    """
+    A function that returns the shape of an array
+    """
+
+    return matrix.shape
