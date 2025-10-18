df3ea122426335d0218f7e8033512fe4f283b7b0
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-18 13:48:30 +0000

    Add: Transposed Matrix -- task solution

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" A script that transposes a matrix"""
+
+
+def np_transpose(matrix):
+    """
+    A function that returns the transpose of a matrix
+    """
+
+    return matrix.T
