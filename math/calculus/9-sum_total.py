9a544c13950067aeed0f0db0d75a5e973290b7f9
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-20 21:37:51 +0000

    Solvig small problems

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,10 +5,11 @@
 def summation_i_squared(n):
     """
     A function that returns the Sum of the Series
+    If n is invalid , returns None
     """
-    if type(n) is not int:
+    if not isinstance(n, int) or n < 1:
         return None
 
-    result = n**3/3 + n**2/2 + n/6
+    result = n * (n + 1) * (2 * n + 1) // 6
 
     return result
