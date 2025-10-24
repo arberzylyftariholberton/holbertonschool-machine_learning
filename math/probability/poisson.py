87b75ea57597719e3ce8df174153302d96cd66ff
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-24 16:31:12 +0000

    Task 2 -- Poisson CDF -- Solved Successfully

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -20,3 +20,25 @@ class Poisson:
                 raise ValueError("data must contain multiple values")
 
             self.lambtha = float(sum(data) / len(data))
+
+    def cdf(self, k):
+        """
+        A function that calculates the CDF for a given number of successes
+        """
+        k = int(k)
+
+        if k < 0:
+            return 0
+
+        e = 2.7182818285
+        cdf_value = 0
+
+        for i in range(k + 1):
+            factorial = 1
+            for j in range(1, i + 1):
+                factorial *= j
+
+            pmf = (e ** (-self.lambtha)) * (self.lambtha ** i) / factorial
+            cdf_value += pmf
+
+        return cdf_value
