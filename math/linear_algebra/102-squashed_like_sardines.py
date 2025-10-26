004a7e893a0a039acdc2933bd93728d6ffeac891
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-26 11:47:06 +0000

    Last Advanced Task -- 102 -- Linear Algebra, hoping to get 100%

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,25 +1,53 @@
 #!/usr/bin/env python3
-""" A script that does concatenation of 2 matrices along a specific axis """
+""" A script that concatenates two matrices along a specific axis """
+
+
+def matrix_shape(matrix):
+    """
+    Returns the shape of a matrix as a list
+    """
+
+    if not isinstance(matrix, list):
+        return []
+
+    shape = [len(matrix)]
+
+    if len(matrix) > 0 and isinstance(matrix[0], list):
+        shape.extend(matrix_shape(matrix[0]))
+
+    return shape
 
 
 def cat_matrices(mat1, mat2, axis=0):
     """
-    A function that returns the concatenation of
-    2 matrices with a specific axis
+    Concatenates two matrices along a specific axis
+    Returning concatenated matrix or None if cannot concatenate
     """
 
-    if axis == 0:
-        return mat1 + mat2
+    shape1 = matrix_shape(mat1)
+    shape2 = matrix_shape(mat2)
 
-    if len(mat1) != len(mat2):
+    if len(shape1) != len(shape2):
         return None
 
-    result = []
+    if axis < 0 or axis >= len(shape1):
+        return None
 
-    for sub_matrix1, sub_matrix2 in zip(mat1, mat2):
-        merged = cat_matrices(sub_matrix1, sub_matrix2, axis=axis-1)
-        if merged is None:
+    for i in range(len(shape1)):
+        if i != axis and shape1[i] != shape2[i]:
             return None
-        result.append(merged)
 
-    return result
+    if axis == 0:
+        if not isinstance(mat1[0], list):
+            return mat1 + mat2
+        else:
+            return [row[:] for row in mat1] + [row[:] for row in mat2]
+
+    else:
+        result = []
+        for i in range(len(mat1)):
+            concatenated = cat_matrices(mat1[i], mat2[i], axis - 1)
+            if concatenated is None:
+                return None
+            result.append(concatenated)
+        return result
