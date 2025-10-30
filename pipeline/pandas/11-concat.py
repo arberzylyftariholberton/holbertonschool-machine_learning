0e7eca69a90178f8c8c7c08af3762b64b99040a1
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-30 19:45:22 +0000

    Task 11- Concatenation -- Still Not Solved -- Problems -- Killed Output

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" A script that concatenates a DataFrame"""
+
+import pandas as pd
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    """
+    A function that returns the concatenated DataFrame
+    """
+
+    df2 = df2[df2["Timestamp"] <= 1417411920]
+
+    df1 = index(df1)
+    df2 = index(df2)
+
+    return pd.concat([df2, df1], keys=["bitstamp", "coinbase"], sort=False)
