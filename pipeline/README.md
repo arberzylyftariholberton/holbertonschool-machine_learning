82c1161e32fc9421921ee82a561a99f5b6e728fb
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-30 17:04:01 +0000

    Changed README with uppercase

---
+++ pipeline/README.md
@@ -0,0 +1,2 @@
+# Read Me
+Testing Read Me
--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -4,8 +4,8 @@
 import pandas as pd
 
 
-def from_file(x, delimiter):
+def from_file(filename, delimiter):
     """
     Function that return loaded data from a file as a pd.DataFrame
     """
-    return pd.read_csv(x, delimiter=delimiter)
+    return pd.read_csv(filename, delimiter=delimiter)
