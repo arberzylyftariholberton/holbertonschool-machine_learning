e4a0f93768661d5f72922ff9419b00249dfb6d9b
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-31 16:21:56 +0000

    Task 13 - Statistics analyzing with describe method-- Solved -- Up to Final Task

---
+++ pipeline/pandas/13-analyze.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" A script that computes descriptive statistics
+    for all columns except the Timestamp column"""
+
+import pandas as pd
+
+
+def analyze(df):
+    """
+    Function that returns the contained DataFrame
+    With descriptive statistics except Timestamp
+    """
+
+    if "Timestamp" in df.columns:
+        df = df.drop(columns=["Timestamp"])
+
+    statistics = df.describe()
+
+    return statistics
