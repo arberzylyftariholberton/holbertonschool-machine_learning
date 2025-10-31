91e98cbb87592efaf20df2a30f4e77b14ca7c8e4
arberzylyftariholberton <12033@holbertonstudents.com>
2025-10-31 15:19:07 +0000

    Task 11- Killed output because of large files

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -13,6 +13,7 @@ def concat(df1, df2):
     df2 = df2[df2["Timestamp"] <= 1417411920]
 
     df1 = index(df1)
+
     df2 = index(df2)
 
     return pd.concat([df2, df1], keys=["bitstamp", "coinbase"], sort=False)
