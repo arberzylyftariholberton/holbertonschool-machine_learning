d2ae7de7866128325f2cf82e47c4da8e22527531
arberzylyftariholberton <12033@holbertonstudents.com>
2025-11-03 22:36:59 +0000

    ylabel naming error

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,13 +13,10 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(
-        student_grades,
-        bins=(np.arange(0, 101, step=10)),
-        edgecolor='black')
+    plt.hist(student_grades, bins=10, range=(0, 101) edgecolor='black')
     plt.title("Project A")
     plt.xlabel("Grades")
-    plt.ylabel("Grades")
+    plt.ylabel("Number of Students")
     plt.xlim(0, 100)
     plt.ylim(0, 30)
     plt.xticks(np.arange(0, 101, step=10))
