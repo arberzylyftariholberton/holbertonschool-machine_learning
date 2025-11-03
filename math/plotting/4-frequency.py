6947f7acb432ef4eebcc3cfe61ec5c28daf03003
arberzylyftariholberton <12033@holbertonstudents.com>
2025-11-03 22:39:43 +0000

    Forgot a comma

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,7 +13,8 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(student_grades, bins=10, range=(0, 101) edgecolor='black')
+    bins = np.arange(0, 101, 10)
+    plt.hist(student_grades, bins=bins, range=(0, 100), edgecolor='black')
     plt.title("Project A")
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
