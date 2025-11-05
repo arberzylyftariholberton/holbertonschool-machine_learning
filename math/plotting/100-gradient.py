0596329df78b7759814ab912000e1b4fbfe8e7d7
arberzylyftariholberton <12033@holbertonstudents.com>
2025-11-05 13:55:03 +0000

    Task 7 -- Advanced Task -- Gradient Scatter

---
+++ math/plotting/100-gradient.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" A sctipt that creates a scatter plot with gradient """
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def gradient():
+    """
+    A function that returns a scatter plot of a elevations of a mountain
+    """
+
+    np.random.seed(5)
+
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    plt.figure(figsize=(6.4, 4.8))
+
+    mountain_elevation = plt.scatter(x, y, c=z)
+    plt.colorbar(mountain_elevation, label='elevation (m)')
+    plt.label = ('Mountain Elevation')
+    plt.ylabel('y coordinate (m)')
+    plt.xlabel('x coordinate (m)')
+
+    plt.show()
