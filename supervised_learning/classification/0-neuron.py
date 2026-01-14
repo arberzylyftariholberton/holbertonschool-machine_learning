fd9b265a7680273cbd942f0c35749952384130b3
arberzylyftariholberton <12033@holbertonstudents.com>
2026-01-14 00:04:22 +0000

    Task 0 -- Solved

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,19 @@
+class Neuron:
+    """
+    A class that defines a single neuron performing binary classification
+    """
+
+
+    def __init__(self, nx):
+        """
+        A constructor that takes number of input as nx
+        """
+
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
