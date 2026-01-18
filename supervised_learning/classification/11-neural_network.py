9328f145eefe06f264d7a808de07029b0c54ea92
arberzylyftariholberton <12033@holbertonstudents.com>
2026-01-18 17:50:42 +0000

    Task 11 -- Add: The Cost Of The Model Using Logistic Regression

---
+++ supervised_learning/classification/11-neural_network.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""
+   A script that Calculates the cost of the model
+   using logistic regression
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    A class that defines a Neural Network performing binary classification,
+    adding private instance attributes
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        A constructor that takes number of input as nx and
+        nodes is the number of nodes found in the hidden layer
+        """
+
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """
+        Getter function of the Weight vector for the hidden layer
+        """
+        return self.__W1
+
+    @property
+    def b1(self):
+        """
+        Getter function of The bias for the hidden layer
+        """
+        return self.__b1
+
+    @property
+    def A1(self):
+        """
+        Getter function of The activated output for the hidden layer
+        """
+        return self.__A1
+
+    @property
+    def W2(self):
+        """
+        Getter function of the Weight vector for the output neuron
+        """
+        return self.__W2
+
+    @property
+    def b2(self):
+        """
+        Getter function of The bias for the output neuron
+        """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """
+        Getter function of The activated output for the output neuron
+        """
+        return self.__A2
+
+    def forward_prop(self, X):
+        """
+        A function that Calculates the forward propagation
+        of the neural network
+        """
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """
+        A Function that Calculates the cost of the model
+        using logistic regression
+        """
+        m = Y.shape[1]
+
+        log_loss = -1/m*np.sum(Y * np.log(A) + (1-Y)*(np.log(1.0000001-A)))
+
+        return log_loss
