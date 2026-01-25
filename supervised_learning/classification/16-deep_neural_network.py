8ffa1a3f39a8ac8b384076cba4f40e349ae8c843
arberzylyftariholberton <12033@holbertonstudents.com>
2026-01-25 22:46:12 +0000

    Task 16 -- Deep Neural Network: Upgrading Documentation

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python3
-"""
-    A script that performs binary classification
-    in a Deep neural network
-"""
-
-
+"""Defines a Deep Neural Network for binary classification."""
 import numpy as np
 
 
 class DeepNeuralNetwork:
     """
-    Deep neural network performing binary classification
+    Represents a deep neural network performing binary classification.
     """
 
     def __init__(self, nx, layers):
         """
-        A constructor that takes number of input as nx and
-        layers is a list representing the number of nodes
-        in each layer of the network
+        Initializes a DeepNeuralNetwork instance.
+
+        Parameters:
+            nx (int): Number of input features.
+            layers (list): List representing the number of nodes in each layer.
+
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is less than 1.
+            TypeError: If layers is not a list of positive integers or is empty.
+
+        Public Attributes:
+            L (int): Number of layers in the neural network.
+            cache (dict): Dictionary to hold all intermediary values of the network, initialized empty.
+            weights (dict): Dictionary to hold all weights and biases of the network. Weights initialized
+                            using He et al. method and biases initialized to zeros.
         """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
@@ -34,7 +42,6 @@ class DeepNeuralNetwork:
         prev = nx
         for layer in range(1, self.L + 1):
             nodes = layers[layer - 1]
-
             if type(nodes) is not int or nodes <= 0:
                 raise TypeError("layers must be a list of positive integers")
 
