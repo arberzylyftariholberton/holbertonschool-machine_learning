dcae2649a81a477d314da4767ca641f93943a2fa
arberzylyftariholberton <12033@holbertonstudents.com>
2026-01-28 16:15:52 +0000

    Task 3 -- Add: Converting a Label Vector Into a one-hot Matrix

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module to convert labels to one-hot matrix"""
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """
+    Converts a label vector into a one-hot matrix
+
+    Args:
+        labels: label vector to convert
+        classes: number of classes (if None, inferred from labels)
+
+    Returns:
+        the one-hot matrix
+    """
+
+    return K.utils.to_categorical(labels, num_classes=classes)
