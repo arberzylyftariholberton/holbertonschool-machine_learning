aa302fbaeb601ba4c5b8c47287e87d8a577e6180
arberzylyftariholberton <12033@holbertonstudents.com>
2026-01-28 16:40:11 +0000

    Task 9 -- Add: Saving And Loading An Entire Model

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Module to save and load Keras models"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """
+    Saves an entire model
+
+    Args:
+        network: the model to save
+        filename: the path of the file that the model should be saved to
+
+    Returns:
+        None
+    """
+
+    network.save(filename)
+    return None
+
+
+def load_model(filename):
+    """
+    Loads an entire model
+
+    Args:
+        filename: the path of the file that the model should be loaded from
+
+    Returns:
+        the loaded model
+    """
+
+    return K.models.load_model(filename)
