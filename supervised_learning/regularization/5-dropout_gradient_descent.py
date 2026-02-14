612ee8eda592d4484b16eb70d3cd475c68f60e8f
arberzylyftariholberton <12033@holbertonstudents.com>
2026-02-14 09:31:38 +0000

    Task 5 -- Changing The Architecture

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -18,16 +18,32 @@ def dropout_create_layer(prev, n, activation, keep_prob, training=True):
         output of the new layer
     """
 
-    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    m = Y.shape[1]
 
-    layer = tf.keras.layers.Dense(
-        units=n,
-        activation=activation,
-        kernel_initializer=initializer
-    )
-    output = layer(prev)
+    A = cache['A' + str(L)]
+    dZ = A - Y
 
-    dropout_layer = tf.keras.layers.Dropout(rate=1 - keep_prob)
-    output = dropout_layer(output, training=training)
+    A_prev = cache['A' + str(L - 1)]
+    W = weights['W' + str(L)]
+    dW = np.matmul(dZ, A_prev.T) / m
+    db = np.sum(dZ, axis=1, keepdims=True) / m
+    dA_prev = np.matmul(W.T, dZ)
 
-    return output
+    weights['W' + str(L)] -= alpha * dW
+    weights['b' + str(L)] -= alpha * db
+
+
+    for layer in range(L - 1, 0, -1):
+        D = cache['D' + str(layer)]
+        dA = dA_prev * (D / keep_prob)
+
+        A = cache['A' + str(layer)]
+        A_prev = cache['A' + str(layer - 1)]
+        dZ = dA * (1 - A ** 2)
+        dW = np.matmul(dZ, A_prev.T) / m
+        db = np.sum(dZ, axis=1, keepdims=True) / m
+        W = weights['W' + str(layer)]
+        dA_prev = np.matmul(W.T, dZ)
+
+        weights['W' + str(layer)] -= alpha * dW
+        weights['b' + str(layer)] -= alpha * db
