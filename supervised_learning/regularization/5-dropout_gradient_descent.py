fe2b65fc148c852908cc4735e921a63c2e3519dc
arberzylyftariholberton <12033@holbertonstudents.com>
2026-02-14 09:47:23 +0000

    Task 5 -- Changes On The Code

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -22,29 +22,33 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
     m = Y.shape[1]
 
-    A = cache['A' + str(L)]
-    dZ = A - Y
+    dZ = cache['A' + str(L)] - Y
 
     A_prev = cache['A' + str(L - 1)]
-    W = weights['W' + str(L)]
     dW = np.matmul(dZ, A_prev.T) / m
     db = np.sum(dZ, axis=1, keepdims=True) / m
-    dA_prev = np.matmul(W.T, dZ)
 
     weights['W' + str(L)] -= alpha * dW
     weights['b' + str(L)] -= alpha * db
 
+    dA_prev = np.matmul(weights['W' + str(L)].T, dZ)
+
     for layer in range(L - 1, 0, -1):
+
         D = cache['D' + str(layer)]
-        dA = dA_prev * (D / keep_prob)
+        dA = dA_prev * D
+        dA = dA / keep_prob
 
         A = cache['A' + str(layer)]
+        dZ = dA * (1 - np.square(A))
+
         A_prev = cache['A' + str(layer - 1)]
-        dZ = dA * (1 - A ** 2)
+
         dW = np.matmul(dZ, A_prev.T) / m
         db = np.sum(dZ, axis=1, keepdims=True) / m
-        W = weights['W' + str(layer)]
-        dA_prev = np.matmul(W.T, dZ)
 
         weights['W' + str(layer)] -= alpha * dW
         weights['b' + str(layer)] -= alpha * db
+
+        if layer > 1:
+            dA_prev = np.matmul(weights['W' + str(layer)].T, dZ)
