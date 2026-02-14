becdcb0b02834f6ebf424092dd038c6450fbb08a
arberzylyftariholberton <12033@holbertonstudents.com>
2026-02-14 09:37:46 +0000

    Task 5 -- Whole File Changed -- Mistake On Code

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,49 +1,52 @@
 #!/usr/bin/env python3
-"""Module to create a neural network layer with dropout in TensorFlow"""
+"""Module for gradient descent with dropout"""
 import numpy as np
 
 
-def dropout_create_layer(prev, n, activation, keep_prob, training=True):
+def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
-    Creates a layer of a neural network using dropout
+    Updates the weights of a neural network with Dropout regularization
+    using gradient descent
 
     Args:
-        prev: tensor containing the output of the previous layer
-        n: number of nodes the new layer should contain
-        activation: activation function for the new layer
+        Y: one-hot numpy.ndarray of shape (classes, m) containing
+           correct labels for the data
+        weights: dictionary of the weights and biases of the neural network
+        cache: dictionary of the outputs and dropout masks of each layer
+        alpha: learning rate
         keep_prob: probability that a node will be kept
-        training: boolean indicating whether the model is in training mode
+        L: number of layers of the network
 
-    Returns:
-        output of the new layer
+    All layers use tanh activation except the last, which uses softmax.
+    The weights of the network should be updated in place.
     """
-
     m = Y.shape[1]
 
-    A = cache['A' + str(L)]
-    dZ = A - Y
-
+    dZ = cache['A' + str(L)] - Y
     A_prev = cache['A' + str(L - 1)]
-    W = weights['W' + str(L)]
     dW = np.matmul(dZ, A_prev.T) / m
     db = np.sum(dZ, axis=1, keepdims=True) / m
-    dA_prev = np.matmul(W.T, dZ)
 
     weights['W' + str(L)] -= alpha * dW
     weights['b' + str(L)] -= alpha * db
 
+    dA_prev = np.matmul(weights['W' + str(L)].T, dZ)
 
     for layer in range(L - 1, 0, -1):
         D = cache['D' + str(layer)]
-        dA = dA_prev * (D / keep_prob)
+        dA = dA_prev * D
+        dA = dA / keep_prob
 
         A = cache['A' + str(layer)]
+        dZ = dA * (1 - np.square(A))
+
         A_prev = cache['A' + str(layer - 1)]
-        dZ = dA * (1 - A ** 2)
+
         dW = np.matmul(dZ, A_prev.T) / m
         db = np.sum(dZ, axis=1, keepdims=True) / m
-        W = weights['W' + str(layer)]
-        dA_prev = np.matmul(W.T, dZ)
 
         weights['W' + str(layer)] -= alpha * dW
         weights['b' + str(layer)] -= alpha * db
+
+        if layer > 1:
+            dA_prev = np.matmul(weights['W' + str(layer)].T, dZ)
