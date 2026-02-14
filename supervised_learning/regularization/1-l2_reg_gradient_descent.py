53c4b418013d45d56a73d75b919d773d15770300
arberzylyftariholberton <12033@holbertonstudents.com>
2026-02-14 09:13:50 +0000

    Tak 1 -- Making Changes

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -28,15 +28,18 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     dZ = cache['A' + str(L)] - Y
 
     for layer in range(L, 0, -1):
+        L2_regularization = lambtha / m * weights['W' + str(layer)]
+
         A_prev = cache['A' + str(layer - 1)]
 
-        dW = (1 / m) * np.matmul(dZ, A_prev.T)
-        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-        dW += (lambtha / m) * weights['W' + str(layer)]
+        dW = np.matmul(dZ, A_prev.T) / m + L2_regularization
+        db = np.sum(dZ, axis=1, keepdims=True) / m
+        dA = np.matmul(weights['W' + str(layer)].T, dZ)
+
+        if layer != 1:
+            dZ = dA * (1 - A_prev ** 2)
+        else:
+            dZ = dA
 
         weights['W' + str(layer)] -= alpha * dW
         weights['b' + str(layer)] -= alpha * db
-
-        if layer > 1:
-            dA = np.matmul(weights['W' + str(layer)].T, dZ)
-            dZ = dA * (1 - np.square(cache['A' + str(layer - 1)]))
