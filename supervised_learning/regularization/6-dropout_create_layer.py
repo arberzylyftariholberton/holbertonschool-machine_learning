db0223809d4567098ae15c7775b3222c3dd02b8e
arberzylyftariholberton <12033@holbertonstudents.com>
2026-02-14 09:50:54 +0000

    Task 6 -- Same Logic -- Different Approach

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -18,7 +18,10 @@ def dropout_create_layer(prev, n, activation, keep_prob, training=True):
         output of the new layer
     """
 
-    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode='fan_avg'
+    )
 
     layer = tf.keras.layers.Dense(
         units=n,
@@ -28,6 +31,7 @@ def dropout_create_layer(prev, n, activation, keep_prob, training=True):
     output = layer(prev)
 
     dropout_layer = tf.keras.layers.Dropout(rate=1 - keep_prob)
-    output = dropout_layer(output, training=training)
 
+    output = dropout_layer(output, training=training)
     return output
+
