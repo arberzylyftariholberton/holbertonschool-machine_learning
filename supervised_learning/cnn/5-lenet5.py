d4d585aefb32df72823e0206b292c83a7b9f7b2f
arberzylyftariholberton <12033@holbertonstudents.com>
2026-03-02 15:22:05 +0000

    Task 4 - Building A Modified LeNet-5 Architecture Using Keras

---
+++ supervised_learning/cnn/5-lenet5.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Module that contains the function lenet5 for building a modified LeNet-5"""
+
+from tensorflow import keras as K
+
+
+def lenet5(X):
+    """
+    Builds a modified LeNet-5 architecture using Keras
+
+    Parameters:
+    X -- K.Input of shape (m, 28, 28, 1)
+
+    Returns:
+    model -- compiled K.Model using Adam optimization and accuracy metrics
+    """
+
+    he_normal = K.initializers.HeNormal(seed=0)
+    A1 = K.layers.Conv2D(6, 5, activation='relu',
+                         kernel_initializer=he_normal, padding='same')(X)
+    A2 = K.layers.MaxPooling2D()(A1)
+    A3 = K.layers.Conv2D(16, 5, activation='relu',
+                         kernel_initializer=he_normal)(A2)
+    A4 = K.layers.MaxPooling2D()(A3)
+    A5 = K.layers.Flatten()(A4)
+    A6 = K.layers.Dense(120, activation='relu',
+                        kernel_initializer=he_normal)(A5)
+    A7 = K.layers.Dense(84, activation='relu',
+                        kernel_initializer=he_normal)(A6)
+    Y = K.layers.Dense(10, activation='softmax',
+                       kernel_initializer=he_normal)(A7)
+    model = K.Model(inputs=X, outputs=Y)
+    model.compile(optimizer=K.optimizers.Adam(),
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
