22c36f934b38d19ab1e9228b7bceff3001297b89
arberzylyftariholberton <12033@holbertonstudents.com>
2026-03-04 22:23:09 +0000

    Fixing Task 100

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -8,38 +8,34 @@ def pca_color(image, alphas):
     Performs PCA color augmentation on an image.
 
     Args:
-        image: 3D tf.Tensor of shape (H, W, 3) - the image to augment
-        alphas: tuple of length 3 - magnitude of change per channel
+        image: 3D tf.Tensor of shape (H, W, 3)
+        alphas: tuple/array of length 3 - per-channel scaling
 
     Returns:
-        Augmented image as a tf.Tensor clipped to [0, 255]
+        Augmented image as tf.uint8 Tensor
     """
-    # Cast to float32 and reshape to (N, 3)
     img = tf.cast(image, tf.float32)
-    shape = tf.shape(img)
     pixels = tf.reshape(img, [-1, 3])
 
-    # Center the pixels
-    mean = tf.reduce_mean(pixels, axis=0)
-    pixels_centered = pixels - mean
+    mean = tf.reduce_mean(pixels, axis=0, keepdims=True)
+    centered = pixels - mean
 
-    # Compute 3x3 covariance matrix: C = (X^T X) / (N - 1)
-    n = tf.cast(tf.shape(pixels_centered)[0], tf.float32)
+    n = tf.cast(tf.shape(centered)[0], tf.float32)
     cov = tf.linalg.matmul(
-        pixels_centered, pixels_centered, transpose_a=True
+        centered, centered, transpose_a=True
     ) / (n - 1.0)
 
-    # Eigendecomposition (eigh for symmetric matrices)
     eigenvalues, eigenvectors = tf.linalg.eigh(cov)
 
-    # AlexNet perturbation: P @ (alphas * lambdas)
-    alphas_tensor = tf.cast(tf.constant(alphas), tf.float32)
-    perturbation = tf.linalg.matvec(
-        eigenvectors, alphas_tensor * eigenvalues
+    alphas_t = tf.cast(
+        tf.convert_to_tensor(alphas, dtype=tf.float64),
+        tf.float32
     )
 
-    # Add perturbation to every pixel and reshape back
-    img_augmented = img + perturbation
-    img_augmented = tf.clip_by_value(img_augmented, 0.0, 255.0)
+    delta = tf.linalg.matvec(
+        eigenvectors, alphas_t * eigenvalues
+    )
 
-    return tf.cast(img_augmented, tf.uint8)
+    augmented = tf.clip_by_value(img + delta, 0.0, 255.0)
+
+    return tf.cast(augmented, tf.uint8)
