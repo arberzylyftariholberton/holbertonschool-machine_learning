45f0404d95a262eb73e3ff5af293943ac49f6e0a
arberzylyftariholberton <12033@holbertonstudents.com>
2026-05-07 13:50:13 +0200

    Fix task 4 FastText import usage

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module that builds and trains a FastText model."""
-from gensim.models import FastText
+import gensim
 
 
 def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
@@ -19,15 +19,15 @@ def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
         workers (int): Number of worker threads.
 
     Returns:
-        FastText: Trained gensim FastText model.
+        gensim.models.FastText: Trained gensim FastText model.
     """
-    model = FastText(sentences=sentences,
-                     vector_size=vector_size,
-                     min_count=min_count,
-                     window=window,
-                     negative=negative,
-                     sg=0 if cbow else 1,
-                     epochs=epochs,
-                     seed=seed,
-                     workers=workers)
+    model = gensim.models.FastText(sentences=sentences,
+                                   vector_size=vector_size,
+                                   min_count=min_count,
+                                   window=window,
+                                   negative=negative,
+                                   sg=0 if cbow else 1,
+                                   epochs=epochs,
+                                   seed=seed,
+                                   workers=workers)
     return model
