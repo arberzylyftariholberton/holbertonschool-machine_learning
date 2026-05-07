4a4c1e9bc51c764863b633241f5f782749daab6d
arberzylyftariholberton <12033@holbertonstudents.com>
2026-05-07 13:50:04 +0200

    Fix task 3 keras import usage

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module that converts a gensim model to a keras embedding layer."""
-from keras.layers import Embedding
+import tensorflow as tf
 
 
 def gensim_to_keras(model):
@@ -10,10 +10,10 @@ def gensim_to_keras(model):
         model: Trained gensim Word2Vec model.
 
     Returns:
-        Embedding: Trainable keras Embedding layer.
+        tf.keras.layers.Embedding: Trainable keras Embedding layer.
     """
     weights = model.wv.vectors
-    return Embedding(input_dim=weights.shape[0],
-                     output_dim=weights.shape[1],
-                     weights=[weights],
-                     trainable=True)
+    return tf.keras.layers.Embedding(input_dim=weights.shape[0],
+                                     output_dim=weights.shape[1],
+                                     weights=[weights],
+                                     trainable=True)
